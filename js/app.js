9af44f8ed7304713a694c9a014be5323c6e867c4
Raylan Araujo <raylanra@gmail.com>
2024-11-15 10:43:02 -0300

    Criar a função REINICIAR

--- js/app.js
+++ js/app.js
@@ -19,7 +19,7 @@ function sortear() {
     let listaSorteio = document.getElementById('lista-sorteio');
 
     for (i = 0 ; i < amigosIncluidos.length; i++){
-        if (i == amigosIncluidos.length -1 ){
+        if (i == amigosIncluidos.length -1 ){ //condição para que o último elemento da lista pegue o primeiro
             listaSorteio.innerHTML += `${amigosIncluidos[i]} --> ${amigosIncluidos[0]} <br>`;
         } else {
             listaSorteio.innerHTML += `${amigosIncluidos[i]} --> ${amigosIncluidos[i+1]} <br>`;
@@ -36,4 +36,10 @@ function embaralha(lista) {
         // atribuição via destructuring
         [lista[indice - 1], lista[indiceAleatorio]] = [lista[indiceAleatorio], lista[indice - 1]];
     }
+}
+
+function reiniciar() {
+    amigosIncluidos = [];
+    document.getElementById('lista-amigos').innerHTML = '';
+    document.getElementById('lista-sorteio').innerHTML = '';
 }
