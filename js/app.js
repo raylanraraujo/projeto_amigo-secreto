e192b99a809f91898492c58ee2a3171423cbb06c
Raylan Araujo <raylanra@gmail.com>
2024-11-15 09:09:08 -0300

    Criar array para armazenar os nomes, incluir o algorítmo de Fisher-Yates, criar a função SORTEAR para embaralhar os nomes

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+let amigosIncluidos = []; //variável que armazena o array dos amigos do sorteio
+
 function adicionar() {
     let amigo = document.getElementById('nome-amigo').value;
     let listaAmigos = document.getElementById('lista-amigos');
@@ -7,6 +9,23 @@ function adicionar() {
     } else {
         listaAmigos.textContent += ', ' + amigo;
     }
-    
     document.getElementById('nome-amigo').value = '';
+
+    amigosIncluidos.push(amigo) //incluir a variável 'AMIGO' no array 'AMIGOS INCLUÍDOS'
+}
+
+function sortear() {
+    embaralha(amigosIncluidos);
+    console.log(amigosIncluidos)
+}
+
+//função embralha é o algoritmo Fisher–Yates
+function embaralha(lista) {
+    for (let indice = lista.length; indice; indice--) {   // indice começa com o tamanho do array
+
+        const indiceAleatorio = Math.floor(Math.random() * indice); // gerou o índice aleatório
+
+        // atribuição via destructuring
+        [lista[indice - 1], lista[indiceAleatorio]] = [lista[indiceAleatorio], lista[indice - 1]];
+    }
 }
